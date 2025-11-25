78e1ff0450789c67679c1b6f651f8f256a4b36f9
Ahmed Abdelbadea <ahmed.abdelbadea@valeo.com>
2025-11-25 13:25:33 +0200

    add host

--- app.py
+++ app.py
@@ -78,4 +78,4 @@ def home():
     )
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5001) # Run on a different port than mlflow ui
+    app.run(debug=True, host='0.0.0.0', port=5001) # Run on a different port than mlflow ui
